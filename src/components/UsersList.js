f8aa631b1a9ddebfc86a605e66b059617947b34e
Grégory Marini <greg.marini@hotmail.fr>
2023-04-26 17:51:48 +0200

    refactor userList component

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -19,25 +19,24 @@ function UsersList() {
     doAddUser();
   };
 
+  let content;
   if (isLoadingUsers) {
-    return <Skeleton times={6} className='h-10 w-full' />;
-  }
-
-  if (loadingUsersError) {
-    return <div>Error fetching data...</div>;
-  }
-
-  const renderedUsers = users.map((user) => (
-    <div key={user.id} className='mb-2 border rounded'>
-      <div className='flex p-2 justify-between items-center cursor-pointer'>
-        {user.name}
+    content = <Skeleton times={6} className='h-10 w-full' />;
+  } else if (loadingUsersError) {
+    content = <div>Error fetching data...</div>;
+  } else {
+    content = users.map((user) => (
+      <div key={user.id} className='mb-2 border rounded'>
+        <div className='flex p-2 justify-between items-center cursor-pointer'>
+          {user.name}
+        </div>
       </div>
-    </div>
-  ));
+    ));
+  }
 
   return (
     <div>
-      <div className='flex flex-row justify-between m-3'>
+      <div className='flex flex-row justify-between items-center m-3'>
         <h1 className='m-2 text-xl'>Users</h1>
         {
           <Button loading={isCreatingUser} onClick={handleUserAdd}>
@@ -46,7 +45,7 @@ function UsersList() {
         }
         {creatingUserError && 'Error creating user...'}
       </div>
-      {renderedUsers}
+      {content}
     </div>
   );
 }
