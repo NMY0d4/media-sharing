8ff045a5ea1f6fead32dd3c461bcbbd7f23ce41a
Grégory Marini <greg.marini@hotmail.fr>
2023-04-26 09:44:01 +0200

    create db.json, add styles component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import './index.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = document.getElementById('root');
-root.render(createRoot(<App />));
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
