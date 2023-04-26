e740c646a9d3944552fb98af7942a112ebaebe33
Grégory Marini <greg.marini@hotmail.fr>
2023-04-26 14:56:44 +0200

    build addUser function(with faker)

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchUsers } from '../store';
+import { fetchUsers, addUser } from '../store';
+import Button from '../components/styles/Button';
 import Skeleton from './styles/Skeleton';
 
 function UsersList() {
@@ -11,6 +12,10 @@ function UsersList() {
     dispatch(fetchUsers());
   }, [dispatch]);
 
+  const handleUserAdd = () => {
+    dispatch(addUser());
+  };
+
   if (isLoading) {
     return <Skeleton times={6} className='h-10 w-full' />;
   }
@@ -19,11 +24,21 @@ function UsersList() {
     return <div>Error fetching data...</div>;
   }
 
+  const renderedUsers = users.map((user) => (
+    <div key={user.id} className='mb-2 border rounded'>
+      <div className='flex p-2 justify-between items-center cursor-pointer'>
+        {user.name}
+      </div>
+    </div>
+  ));
+
   return (
     <div>
-      {users.map((user) => (
-        <div key={user.id}> {user.name}</div>
-      ))}
+      <div className='flex flex-row justify-between m-3'>
+        <h1 className='m-2 text-xl'>Users</h1>
+        <Button onClick={handleUserAdd}>+ Add User</Button>
+      </div>
+      {renderedUsers}
     </div>
   );
 }
--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -5,7 +5,7 @@ export const fetchUsers = createAsyncThunk('users/fetch', async () => {
   const response = await axios.get(`http://localhost:3005/users`);
 
   // DEV ONLY!!!
-  await pause(10);
+  await pause(2);
 
   return response.data;
 });
