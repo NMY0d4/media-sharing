98b5286a2abaa031ab4958aac8e40bf4b50f2ec0
Grégory Marini <greg.marini@hotmail.fr>
2023-04-28 12:17:39 +0200

    build albums element with RTK

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const albumsApi = createApi({
   reducerPath: 'albums',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3005'
+    baseUrl: 'http://localhost:3005',
   }),
   endpoints(builder) {
     return {
@@ -12,12 +12,15 @@ const albumsApi = createApi({
           return {
             url: '/albums',
             params: {
-              userId: user.id 
+              userId: user.id,
             },
-            method: 'GET'
-          }
-        }
-      })
-    }
-  }
-})
+            method: 'GET',
+          };
+        },
+      }),
+    };
+  },
+});
+
+export const { useFetchAlbumsQuery } = albumsApi;
+export { albumsApi };
