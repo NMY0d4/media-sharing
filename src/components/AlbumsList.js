6a57fb0130881f41206d709d53c958d796c22a9c
Grégory Marini <greg.marini@hotmail.fr>
2023-04-30 10:44:13 +0200

    add a pause fn in albumsApi to test the spinner

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -7,14 +7,14 @@ import Button from './styles/Button';
 function AlbumsList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
   const [addAlbum, results] = useAddAlbumMutation();
-  
+
   const handleAddAlbum = () => {
     addAlbum(user);
   };
 
   let content;
   if (isLoading) {
-    content = <Skeleton times={3} />;
+    content = <Skeleton className='h-10 w-full' times={3} />;
   } else if (error) {
     content = <div>Error loading albums.</div>;
   } else {
@@ -32,7 +32,9 @@ function AlbumsList({ user }) {
     <div>
       <div className='m-2 flex flex-row items-center justify-between'>
         <h3 className='text-lg font-bold'>Albums for {user.name}</h3>
-        <Button loading={results.isLoading} onClick={handleAddAlbum}>+ Album</Button>
+        <Button loading={results.isLoading} onClick={handleAddAlbum}>
+          + Album
+        </Button>
       </div>
 
       <div>{content}</div>
